96477ecbafe81a94bb9a88534c3a17d91482ac3b
Greg Hellings <greg@thesub.net>
2016-12-21 13:09:32 -0800

    Fixing myentrypoint script

--- myentrypoint.sh
+++ myentrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #set -e
 
-NUM=`echo $HOSTNAME |sed -e 's/.*-\([0-9]\{1,\}\)$/\1/'`
-NODE_VAR="HAPROXY_$NUM_ENV_DOCKERCLOUD_CONTAINER_HOSTNAME"
-PORT_VAR="HAPROXY_$NUM_ENV_STATS_PORT"
+export NUM=`echo $HOSTNAME |sed -e 's/.*-\([0-9]\{1,\}\)$/\1/'`
+export NODE_VAR="HAPROXY_${NUM}_ENV_DOCKERCLOUD_CONTAINER_HOSTNAME"
+export PORT_VAR="HAPROXY_${NUM}_ENV_STATS_PORT"
 export HAPROXY_HOST=${!NODE_VAR}
 export HAPROXY_PORT=${!PORT_VAR}
 
